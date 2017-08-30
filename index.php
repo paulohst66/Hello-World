4c26f7e2bc19fdf3bd77f73907e7f0bea1b11913
paulohst66 <30348225+paulohst66@users.noreply.github.com>
2017-08-30 15:27:26 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,9 +2,9 @@
 
 echo "Hello World!";
 echo "\n <h1>first update</h1>\n"
-echo "<body>"
-echo "<h2>Body</h2>"
+#echo "<body>"
+#echo "<h2>Body</h2>"
 #echo '<img src="IMG_4829.PNG">'
-echo "</body>"
+#echo "</body>"
 
 ?>
