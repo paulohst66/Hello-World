4886d1027a0de0510bd2420333ac031f20110e6d
paulohst66 <30348225+paulohst66@users.noreply.github.com>
2017-08-30 15:13:00 -0300

    Update index.php

--- index.php
+++ index.php
@@ -2,7 +2,9 @@
 
 echo "Hello World!";
 echo "\n <h1>first update</h1>\n"
-#echo '<img src="/img_4508.jpg" width="280" height="125" title="Olaf" alt="Logo of a company" />'
+$jpeg = imagecreatefromjpeg('./img_4508.jpg');
+
+echo '<img src="./img_4508.jpg" title="Olaf" alt="Logo of a company" />'
 
 
 ?>
