1dab108607094d0907aa8e3eb0381445e2088933
paulohst66 <30348225+paulohst66@users.noreply.github.com>
2017-08-30 15:03:37 -0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,8 @@
 <?php
 
 echo "Hello World!";
-echo "\n <h1>first update</h1>"
+echo "\n <h1>first update</h1>\n"
+echo '<img src="/img_4508.jpg" width="280" height="125" title="Olaf" alt="Logo of a company" />'
+
 
 ?>
